52fcde876b8534e896ec456b714626f40932995a
himanshup18 <himanshu123lmp@gmail.com>
2024-10-08 18:42:12 +0530

    Done

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,6 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+const host = "https://jobportal-e9ax.onrender.com";
+
+export const USER_API_END_POINT = `${host}/api/v1/user`;
+export const JOB_API_END_POINT = `${host}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${host}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${host}/api/v1/company`;
